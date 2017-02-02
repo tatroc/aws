e348feec5812209cad8a7214415064c2d32414ef
Chris Tatro <tatroc@gmail.com>
2017-02-02 04:08:26 -0600

    test

--- ec2.sh
+++ ec2.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-aws cloudformation deploy --region us-east-1 --template-file /c/Users/tatroc/Documents/aws/EC2_Instance_With_Ephemeral_Drives.json --stack-name my-new-stack  --parameter-overrides KeyName=ctatro,InstanceType=t1.micro,SSHLocation=0.0.0.0/0
+clear
+aws cloudformation deploy --region us-east-1 --template-file /c/Users/tatroc/Documents/aws/EC2_Instance_With_Ephemeral_Drives.json --stack-name my-new-stack  --parameter-overrides KeyName=ctatro
+#,InstanceType=t1.micro,SSHLocation="0.0.0.0/0"
 aws cloudformation describe-stack-events --region us-east-1 --stack-name my-new-stack
