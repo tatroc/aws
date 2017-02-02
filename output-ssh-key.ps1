c8e2792b779eb27fc3687a9ff94cbced32540162
Chris Tatro <tatroc@gmail.com>
2017-02-02 05:22:07 -0600

    powershell script

---
+++ output-ssh-key.ps1
@@ -0,0 +1,9 @@
+﻿$aws_keys = Get-Content -Raw -Path "C:\Users\tatroc\Documents\aws_keys\ctatro_aws_keypair.json" | ConvertFrom-Json
+
+
+$keyMaterial = $aws_keys.KeyMaterial
+
+$key = $keyMaterial.Replace("\n","")
+Write-host $key
+
+$key | Out-File "C:\Users\tatroc\Documents\aws_keys\ctatro.key"
