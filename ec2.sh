a0ebc87d7dde579f9799c312806ef46219f869b4
Chris Tatro <tatroc@gmail.com>
2017-02-02 04:56:45 -0600

    working cloud formation command

--- ec2.sh
+++ ec2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 clear
-aws cloudformation deploy --region us-east-1 --template-file /c/Users/tatroc/Documents/aws/EC2_Instance_With_Ephemeral_Drives.json --stack-name my-new-stack  --parameter-overrides KeyName=ctatro
-#,InstanceType=t1.micro,SSHLocation="0.0.0.0/0"
-aws cloudformation describe-stack-events --region us-east-1 --stack-name my-new-stack
+#aws cloudformation validate-template --region us-east-1 --template-body /c/Users/tatroc/Documents/aws/EC2_Instance_With_Ephemeral_Drives.json 
+aws cloudformation deploy --region us-east-1 --template-file /c/Users/tatroc/Documents/aws/EC2_Instance_With_Ephemeral_Drives.json --stack-name my-new-stack-2  --parameter-overrides KeyName=ctatro InstanceType=t1.micro SSHLocation="0.0.0.0/0"
+#aws cloudformation describe-stack-events --region us-east-1 --stack-name my-new-stack
